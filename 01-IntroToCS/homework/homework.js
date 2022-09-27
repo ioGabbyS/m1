2d07ad08a9419eb317fb808ba065aa42018afe31
ioGabbyS <iogabbys@gmail.com>
2022-09-27 01:39:22 -0300

    m1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,18 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+ // num.toString(2);
+  let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
 }
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  parseInt("111", 2);
 }
 
 
